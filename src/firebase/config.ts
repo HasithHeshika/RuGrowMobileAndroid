5f348e10a667d1e9b8bcd129e679d552af760268
Hasith Heshika <hasithheshika2001amp@gmail.com>
2025-11-25 13:47:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8834724498-f8cc4",
+  "appId": "1:639835871581:web:f2a594db0b5efa9d3b30b4",
+  "apiKey": "AIzaSyB1V0T46PDfrwHJfHxKlMPBFqV_nhn8zfc",
+  "authDomain": "studio-8834724498-f8cc4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "639835871581"
+};
